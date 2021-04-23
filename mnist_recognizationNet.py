5963726bdecbff8944bfb728f8ad448c672b05fd
Kevin <zhengyuxiang8888@gmail.com>
2021-04-23 22:05:45 +0800

    mnist_recognizationNet 1st version

--- mnist_recognizationNet.py
+++ mnist_recognizationNet.py
@@ -2,6 +2,8 @@ import sys, os
 sys.path.append(os.pardir)
 from common.functions import *
 from common.gradient import numerical_gradient
+from dataset.mnist import load_mnist
+
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
@@ -45,3 +47,45 @@ class TwoLayerNet:
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
         return grads
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# 超参数
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+iter_per_epoch = max(train_size / batch_size, 1)
+for i in range(iters_num):
+    # get mini-batch
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # calculate gradient
+    grad = network.numerical_gradient(x_batch, t_batch)
+    # grad = network.gradient(x_batch, t_batch)
+
+    # update parameter
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    # record learning process
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    # calculate the accuracy of each epoch
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_loss_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+
+
