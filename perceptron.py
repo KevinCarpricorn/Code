17929cb306a42e7887c424bb9f102a5166403921
Kevin <zhengyuxiang8888@gmail.com>
2021-04-23 22:04:33 +0800

    2nd version of perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
@@ -10,15 +11,17 @@ def AND(x1, x2):
     else:
         return 1
 
+
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
     b = 0.7
     tmp = np.sum(x * w) + b
     if tmp <= 0:
-     return 0
+        return 0
     else:
-     return 1
+        return 1
+
 
 def OR(x1, x2):
     x = np.array([x1, x2])
@@ -30,10 +33,12 @@ def OR(x1, x2):
     else:
         return 1
 
+
 def XOR(x1, x2):
     s1 = NAND(x1, x2)
     s2 = OR(x1, x2)
     y = AND(s1, s2)
     return y
+
 
 print(XOR(1, 1))
