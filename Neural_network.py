1ada170cc7267821111814ccc4a5a2c8dab32b48
Kevin <zhengyuxiang8888@gmail.com>
2021-04-23 22:03:15 +0800

    second version of Neural_network

--- Neural_network.py
+++ Neural_network.py
@@ -13,8 +13,41 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0, x)
 
-x = np.arange(-5, 5, 0.1)
-y = relu(x)
-plt.plot(x, y)
-plt.ylim(-1, 5)
-plt.show()
+
+def identity_function(x):
+    return x
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+
